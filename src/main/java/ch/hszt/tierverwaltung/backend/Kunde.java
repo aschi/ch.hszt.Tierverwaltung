c19e7afedbc63813d94fd03f4d87a9629ae4b740
Adrian Schmid <schmiad1@students.zhaw.ch>
2011-12-15 09:39:40 +0100

    JavaDoc für Kunde hinzugefügt

--- src/main/java/ch/hszt/tierverwaltung/backend/Kunde.java
+++ src/main/java/ch/hszt/tierverwaltung/backend/Kunde.java
@@ -15,6 +15,9 @@ public class Kunde implements IDataRecord {
 	private String eMail;
 	private ArrayList<Tier> tiere;
 
+	/**
+	 * Leerer Konstruktor. Initialisiert alle Werte
+	 */
 	public Kunde() {
 		name = "";
 		vorname = "";
@@ -26,6 +29,16 @@ public class Kunde implements IDataRecord {
 		tiere = new ArrayList<Tier>();
 	}
 
+	/**
+	 * Konstruktor zum direkten setzen aller Felder (ausgenommen id & tierliste)
+	 * @param name
+	 * @param vorname
+	 * @param adresse
+	 * @param plz
+	 * @param ort
+	 * @param telefon
+	 * @param eMail
+	 */
 	public Kunde(String name, String vorname, String adresse, String plz,
 			String ort, String telefon, String eMail) {
 		super();
@@ -39,6 +52,17 @@ public class Kunde implements IDataRecord {
 		tiere = new ArrayList<Tier>();
 	}
 
+	/**
+	 * Konstruktor zum direkten setzen aller Felder (inklusive id, ausgenommen tierliste)
+	 * @param id
+	 * @param name
+	 * @param vorname
+	 * @param adresse
+	 * @param plz
+	 * @param ort
+	 * @param telefon
+	 * @param eMail
+	 */
 	public Kunde(int id, String name, String vorname, String adresse,
 			String plz, String ort, String telefon, String eMail) {
 		this(name, vorname, adresse, plz, ort, telefon, eMail);
@@ -46,12 +70,34 @@ public class Kunde implements IDataRecord {
 		tiere = new ArrayList<Tier>();
 	}
 
+	/**
+	 * Konstruktor zum direkten setzen aller Felder (ausgenommen id, inklusive tierliste)
+	 * @param name
+	 * @param vorname
+	 * @param adresse
+	 * @param plz
+	 * @param ort
+	 * @param telefon
+	 * @param eMail
+	 * @param tiere
+	 */
 	public Kunde(String name, String vorname, String adresse, String plz,
 			String ort, String telefon, String eMail, ArrayList<Tier> tiere) {
 		this(name, vorname, adresse, plz, ort, telefon, eMail);
 		this.tiere = tiere;
 	}
 
+	/**
+	 * Konstruktor zum direkten setzen aller Felder (inklusive id, inklusive tierliste)
+	 * @param name
+	 * @param vorname
+	 * @param adresse
+	 * @param plz
+	 * @param ort
+	 * @param telefon
+	 * @param eMail
+	 * @param tiere
+	 */
 	public Kunde(int id, String name, String vorname, String adresse,
 			String plz, String ort, String telefon, String eMail,
 			ArrayList<Tier> tiere) {
@@ -59,6 +105,9 @@ public class Kunde implements IDataRecord {
 		this.tiere = tiere;
 	}
 
+	/**
+	 * Prüft das aktuelle Kundenobjekt auf seine Gültigkeit. Wirft eine ValidationException falls nicht
+	 */
 	@Override
 	public void validate() throws ValidationException {
 		ValidationException ve = new ValidationException();
@@ -96,78 +145,153 @@ public class Kunde implements IDataRecord {
 		}
 	}
 
+	/**
+	 * Gibt die Kunden Id zurück
+	 * @return id des Kunden
+	 */
 	public int getKundeID() {
 		return kundeID;
 	}
 
+	/**
+	 * Setzt die Kunden Id
+	 * @param kundeID Neue Id
+	 */
 	public void setKundeID(int kundeID) {
 		this.kundeID = kundeID;
 	}
 
+	/**
+	 * Gibt den Namen des Kunden zurück
+	 * @return Name des Kunden
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Setzt den Namen des Kunden
+	 * @param name Neuer Name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gibt den Vornamen des Kunden zurück
+	 * @return Vorname des Kunden
+	 */
 	public String getVorname() {
 		return vorname;
 	}
 
+	/**
+	 * Setzt den Vornamen des Kunden
+	 * @param vorname Neuer Vorname
+	 */
 	public void setVorname(String vorname) {
 		this.vorname = vorname;
 	}
 
+	/**
+	 * Gibt die Adresse des Kunden zurück
+	 * @return Adresse des Kunden
+	 */
 	public String getAdresse() {
 		return adresse;
 	}
 
-	public void setAdresse(String addresse) {
-		this.adresse = addresse;
+	/**
+	 * Setzt die Adresse des Kunden
+	 * @param addresse Neue Adresse
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
 	}
 
+	/**
+	 * Gibt die PLZ des Kunden zurück
+	 * @return PLZ des Kunden
+	 */
 	public String getPlz() {
 		return plz;
 	}
 
+	/**
+	 * Setzt die PLZ des Kunden
+	 * @param plz Neue PLZ
+	 */
 	public void setPlz(String plz) {
 		this.plz = plz;
 	}
 
+	/**
+	 * Gibt den Wohnort des Kunden zurück
+	 * @return Wohnort des Kunden
+	 */
 	public String getOrt() {
 		return ort;
 	}
 
+	/**
+	 * Setzt den Wohnort des Kunden
+	 * @param ort Neuer Wohnort
+	 */
 	public void setOrt(String ort) {
 		this.ort = ort;
 	}
 
+	/**
+	 * Gibt die Telefon Nr des Kunden zurück
+	 * @return Telefon Nr des Kunden
+	 */
 	public String getTelefon() {
 		return telefon;
 	}
 
+	/**
+	 * Setzt die Telefon Nr des Kunden
+	 * @param telefon Neue Telefon Nr
+	 */
 	public void setTelefon(String telefon) {
 		this.telefon = telefon;
 	}
 
+	/**
+	 * Gibt die Email Adresse des Kunden zurück
+	 * @return Email Adresse des Kunden
+	 */
 	public String getEMail() {
 		return eMail;
 	}
 
+	/**
+	 * Setzt die Email Adresse des Kunden
+	 * @param eMail Neue Email Adresse
+	 */
 	public void setEMail(String eMail) {
 		this.eMail = eMail;
 	}
 
+	/**
+	 * Gibt eine Liste der dem Kunden zugewiesenen Tiere zurück
+	 * @return Liste der dem Kunden zugewiesenen Tiere
+	 */
 	public ArrayList<Tier> getTiere() {
 		return tiere;
 	}
 
+	/**
+	 * Überschreibt die Liste der dem Kunden zugewiesenen Tiere
+	 * @param tiere Neue Liste
+	 */
 	public void setTiere(ArrayList<Tier> tiere) {
 		this.tiere = tiere;
 	}
 
+	/**
+	 * Erstellt eine exakte kopie des Kundenobjekts
+	 */
 	@Override
 	public Object clone() {
 		return new Kunde(kundeID, new String(name), new String(vorname),
@@ -175,6 +299,9 @@ public class Kunde implements IDataRecord {
 				new String(eMail), (ArrayList<Tier>) tiere.clone());
 	}
 
+	/**
+	 * Gibt die Id des Kunden zurück
+	 */
 	@Override
 	public int getID() {
 		return kundeID;
